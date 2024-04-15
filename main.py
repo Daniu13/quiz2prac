a9def9f6e2b90662c63848612b75b76927d5d0f3
Daniu13 <daniel.lozano1@udea.edu.co>
2024-04-15 17:19:54 -0500

    creation1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Hi
+def main():
+    while True:
+        menu = int(input("""Opciones:
+                         \n1. Ingresar MAT
+                         \n2. Ingresar CSV
+                         \n3. Graficar señal
+                         \n4. Mostrar información
+                         \n5. Salir"""))
+
+if __name__ = '__main__':
+    main()
